67c4568e3e5b250a62188b116dbcebd7329f3fac
FabioMs27 <41649726+FabioMs27@users.noreply.github.com>
2019-10-26 18:27:32 -0300

    classes setup

---
+++ miniProjeto/miniProjeto/src/miniProjeto/Cozinheiro.java
@@ -0,0 +1,19 @@
+package miniProjeto;
+
+public class Cozinheiro extends Funcionario{
+
+	
+	//Construtor
+	Cozinheiro(String nome, String id) {
+		super(nome, id);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Métodos
+	@Override
+	public void acessarRestaurante() {
+		// TODO Auto-generated method stub
+		super.acessarRestaurante();
+	}
+
+}
---
+++ miniProjeto/miniProjeto/src/miniProjeto/Manager.java
@@ -0,0 +1,19 @@
+package miniProjeto;
+
+public class Manager extends Funcionario{
+
+	//Atributos
+	
+	//Construtor
+	Manager(String nome, String id) {
+		super(nome, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	//Métodos
+	@Override
+	public void acessarRestaurante() {
+		// TODO Auto-generated method stub
+		super.acessarRestaurante();
+	}
+}
